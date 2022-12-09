db84893415366cb2dc652cbb3c5d3b3b82412d90
Víctor Manuel Gamarra Rodríguez <beorn57@hotmail.com>
2022-12-09 21:53:20 +0100

    09/12/2022

--- src/main/java/com/tiendavirtual/controllers/CategoriaController.java
+++ src/main/java/com/tiendavirtual/controllers/CategoriaController.java
@@ -1,16 +1,8 @@
 package com.tiendavirtual.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import com.tiendavirtual.DAO.CategoriaDAO;
-import com.tiendavirtual.DAO.RolDAO;
 import com.tiendavirtual.modelos.Categoria;
-import com.tiendavirtual.modelos.Producto;
-import com.tiendavirtual.modelos.Rol;
-import com.tiendavirtual.modelos.Subcategoria;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,19 +21,41 @@ public class CategoriaController {
             return categorias1;
     }
 
+    @RequestMapping(value="/findCategoriaByIdAdmin/{id}")
+    @ResponseBody
+    public Categoria findCategoriaById(@PathVariable Integer id)
+    {
+        Categoria categoria2=categoriaDAO.findCategoriaById(id);
+        return categoria2;
+    }
+    @RequestMapping(value="/crearCategoriAdmin/{categoria}", method=RequestMethod.POST)
+    @ResponseBody
+    public Integer crearCategoria(@PathVariable Categoria categoria)
+    {
+        int creado=categoriaDAO.addCategoria(categoria);
+        return creado;
+    }
     @RequestMapping(value="/categoriasAdmin", method=RequestMethod.GET)
     public String getAllCategoriasAdmin(Model model)
     {
         ArrayList<Categoria> listaCategoriasAdmin= (ArrayList<Categoria>) categoriaDAO.getAllCategoria();
         model.addAttribute("listaCategoriasAdmin", listaCategoriasAdmin);
         return "categorias";
-
     }
 
-    @RequestMapping(value="/borrarCategoriaAdmin" method=RequestMethod.DELETE)
+    @RequestMapping(value="/borrarCategoriaAdmin/{id}", method=RequestMethod.DELETE)
     @ResponseBody
-    public Categoria borrarCategoria (@PathVariable Integer id)
+    public Integer borrarCategoria (@PathVariable Integer id)
     {
+        int borrado=categoriaDAO.deleteCategoria(id);
+        return borrado;
+    }
 
+    @RequestMapping(value="/actualizarCategoriasAdmin/{categoria]", method=RequestMethod.POST)
+    @ResponseBody
+    public Integer actualizarCategoria(@PathVariable Categoria categoria)
+    {
+        int actualizado=categoriaDAO.actualizarCategoria(categoria);
+        return actualizado;
     }
     }
